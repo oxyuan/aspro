57b46b09c73bad8a8cd5e71b0faef6bb94d651ed
oxyuan <oxyuan@outlook.com>
2022-04-18 13:27:46 +0800

    feat: initial bootstrap class.

---
+++ aspro-starter/aspro-starter-gateway/src/main/java/org/atbyuan/aspro/gateway/AsproGatewayApplication.java
@@ -0,0 +1,22 @@
+package org.atbyuan.aspro.gateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author atbyuan
+ * @since 2022/4/18 13:19
+ */
+@Slf4j
+@EnableDiscoveryClient
+@SpringBootApplication
+public class AsproGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsproGatewayApplication.class, args);
+        log.info("=====>>> aspro-gateway start success <<<=====");
+    }
+
+}
