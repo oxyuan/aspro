43418686bedc82c0a9b193eefffb048832cb43ff
oxyuan <oxyuan@outlook.com>
2022-04-27 23:55:25 +0800

    enabled asynchronous execute.

--- aspro-service/aspro-service-engine/src/main/java/org/atbyuan/aspro/EngineAsproApplication.java
+++ aspro-service/aspro-service-engine/src/main/java/org/atbyuan/aspro/EngineAsproApplication.java
@@ -3,14 +3,12 @@ package org.atbyuan.aspro;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * @author atbyuan
  * @since 2022/4/25 09:25
  */
 @Slf4j
-@EnableAsync
 @SpringBootApplication
 public class EngineAsproApplication {
 
